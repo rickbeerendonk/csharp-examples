f54f5e1352887993a501fb53db3dfa772985ea8b
Rick Beerendonk <rick@beerendonk.com>
2022-08-27 12:32:49 +0200

    Add "Comments" example.

---
+++ 02 - Comments/Program.cs
@@ -0,0 +1,20 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+// One line
+
+Console.WriteLine("1");
+
+/*
+Multi
+line
+*/
+
+Console.WriteLine("2");
+
+/*
+Multi
+  // Nested line
+*/
+
+Console.WriteLine("3");
