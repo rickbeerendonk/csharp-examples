2c2c03ee60d94c5c5df76a577711cadd532c8793
Rick Beerendonk <rick@beerendonk.com>
2022-08-29 14:02:06 +0200

    Add "Variables - var" example.

---
+++ 03 - Variables/a. var/Program.cs
@@ -0,0 +1,16 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2022 Rick Beerendonk
+
+// Explicit type
+int expl = 123;
+
+// Implicit type
+var impl = 456;
+
+Console.WriteLine(expl.GetType()); // System.Int32
+Console.WriteLine(impl.GetType()); // System.Int32
+
+// No error (as opposed to const)
+expl = impl;
+
+Console.WriteLine(expl); // 456
