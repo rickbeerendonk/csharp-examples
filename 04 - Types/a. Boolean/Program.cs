d0dcb3d5606a63a14c7128fd743e14f081b40687
Rick Beerendonk <rick@beerendonk.com>
2023-10-04 21:03:49 +0200

    Add "Types - Boolean" example.

---
+++ 04 - Types/a. Boolean/Program.cs
@@ -0,0 +1,7 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+Console.WriteLine(typeof(bool).FullName); // System.Boolean
