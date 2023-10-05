9edbb969a885bf1c0b4820afcc9d1f2b97f6f5a4
Rick Beerendonk <rick@beerendonk.com>
2023-10-05 21:09:39 +0200

    Add "Types - byte" example.

---
+++ 04 - Types/b2. sbyte/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(byte.MinValue); // 0
+Console.WriteLine(byte.MaxValue); // 255
+
+// Explicit vs Implicit
+byte explicitValue = 255;
+var implicitValue = 255; // int
+
+Console.WriteLine(explicitValue.GetType().FullName); // System.Byte
+Console.WriteLine(implicitValue.GetType().FullName); // System.Int32
