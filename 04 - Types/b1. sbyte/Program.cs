88d7bad7235dacb7aa6cc74b633953c4a7d2b122
Rick Beerendonk <rick@beerendonk.com>
2023-10-05 21:06:29 +0200

    Add "Types - sbyte" example.

---
+++ 04 - Types/b1. sbyte/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(sbyte.MinValue); // -128
+Console.WriteLine(sbyte.MaxValue); // 127
+
+// Explicit vs Implicit
+sbyte explicitValue = 127;
+var implicitValue = 127; // int
+
+Console.WriteLine(explicitValue.GetType().FullName); // System.SByte
+Console.WriteLine(implicitValue.GetType().FullName); // System.Int32
