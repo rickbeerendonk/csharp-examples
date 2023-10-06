fd665b92d04977efc58111fdbce411d36a1f5052
Rick Beerendonk <rick@beerendonk.com>
2023-10-06 15:11:57 +0200

    Add full implementation example.

--- 16 - Record/a. Define/Program.cs
+++ 16 - Record/a. Define/Program.cs
@@ -2,12 +2,12 @@
 // Copyright © 2023 Rick Beerendonk
 
 // Create
-Demo demo1 = new Demo(1, "two");
-Demo demo2 = new Demo(1, "two");
+Demo demo1 = new(1, "two");
+Demo demo2 = new(1, "two");
 
 // Generated Properties
-Console.WriteLine(demo1.one);                // 1
-Console.WriteLine(demo1.two);                // two
+Console.WriteLine(demo1.One);                // 1
+Console.WriteLine(demo1.Two);                // two
 
 // Supported methods (open):
 Console.WriteLine(demo1.Equals(demo2));      // True
@@ -20,7 +20,22 @@ Console.WriteLine(one);       // 1
 Console.WriteLine(two);       // two
 
 // Supported methods (nondestructive mutation):
-Console.WriteLine(demo1 with { two = "new" });  // Demo { one = 1, two = new }
+Console.WriteLine(demo1 with { Two = "new" });  // Demo { one = 1, two = new }
 
 // Define
-public record Demo(int one, string two);
+public record Demo(int One, string Two);
+
+// Define (full implementation)
+/*
+public record Demo
+{
+  public int One { get; init; }
+  public string Two { get; init; }
+
+  public Demo(int one, string two)
+      => (One, Two) = (one, two);
+
+  public void Deconstruct(out int one, out string two)
+      => (one, two) = (One, Two);
+}
+*/
