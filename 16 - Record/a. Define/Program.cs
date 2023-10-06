1ba4bdb2a41aa40f11f7c9f532b29c107074a38b
Rick Beerendonk <rick@beerendonk.com>
2023-10-06 14:14:09 +0200

    Add "Record - Define" example.

---
+++ 16 - Record/a. Define/Program.cs
@@ -0,0 +1,26 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+// Create
+Demo demo1 = new Demo(1, "two");
+Demo demo2 = new Demo(1, "two");
+
+// Generated Properties
+Console.WriteLine(demo1.one);                // 1
+Console.WriteLine(demo1.two);                // two
+
+// Supported methods (open):
+Console.WriteLine(demo1.Equals(demo2));      // True
+Console.WriteLine(demo1.GetHashCode());      // 456035898
+Console.WriteLine(demo1.ToString());         // Demo { one = 1, two = two }
+
+// Supported methods (destrcuturing):
+var (one, two) = demo1;
+Console.WriteLine(one);       // 1
+Console.WriteLine(two);       // two
+
+// Supported methods (nondestructive mutation):
+Console.WriteLine(demo1 with { two = "new" });  // Demo { one = 1, two = new }
+
+// Define
+public record Demo(int one, string two);
