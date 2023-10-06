0bd48c1eae2621c9a8713d1617bc9c1ef71221bd
Rick Beerendonk <rick@beerendonk.com>
2023-10-06 15:15:55 +0200

    Add "Types - short" example.

---
+++ 04 - Types/c1. short/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(short.MinValue); // -32768
+Console.WriteLine(short.MaxValue); // 32767
+
+// Explicit vs Implicit
+short explicitValue = 32767;
+var implicitValue = 32767; // int
+
+Console.WriteLine(explicitValue.GetType().FullName); // System.Int16
+Console.WriteLine(implicitValue.GetType().FullName); // System.Int32
