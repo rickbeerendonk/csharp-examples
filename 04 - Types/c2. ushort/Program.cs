86b6633c0b56077127f71a6e7997e6dbea666efc
Rick Beerendonk <rick@beerendonk.com>
2023-10-06 15:17:33 +0200

    Add "Types - ushort" example.

---
+++ 04 - Types/c2. ushort/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(ushort.MinValue); // 0
+Console.WriteLine(ushort.MaxValue); // 65535
+
+// Explicit vs Implicit
+ushort explicitValue = 65535;
+var implicitValue = 65535; // int
+
+Console.WriteLine(explicitValue.GetType().FullName); // System.UInt16
+Console.WriteLine(implicitValue.GetType().FullName); // System.Int32
