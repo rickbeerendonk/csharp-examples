0e23ab55ebcfdecca1e87d722f238ccd1112c4d3
Rick Beerendonk <rick@beerendonk.com>
2023-10-07 20:05:41 +0200

    Add "Types - Int" examples.

---
+++ 04 - Types/d1. int/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(int.MinValue); // --2147483648
+Console.WriteLine(int.MaxValue); // 2147483647
+
+// Explicit vs Implicit
+int explicitValue = 2147483647;
+var implicitValue = 2147483647;
+
+Console.WriteLine(explicitValue.GetType().FullName); // System.Int32
+Console.WriteLine(implicitValue.GetType().FullName); // System.Int32
