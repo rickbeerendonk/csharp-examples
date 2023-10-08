edd066b119128ec12d679d292b460be4c3ec19a9
Rick Beerendonk <rick@beerendonk.com>
2023-10-08 20:07:29 +0200

    Add "Types - UInt" example.

---
+++ 04 - Types/d2. uint/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(uint.MinValue); // 0
+Console.WriteLine(uint.MaxValue); // 4294967295
+
+// Explicit vs Implicit
+uint explicitValue = 4294967295;
+var implicitValue = 4294967295;  // uint
+
+Console.WriteLine(explicitValue.GetType().FullName); // System.UInt32
+Console.WriteLine(implicitValue.GetType().FullName); // System.UInt32
