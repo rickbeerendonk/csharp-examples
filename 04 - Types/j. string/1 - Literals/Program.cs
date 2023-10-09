e457ba4b5aaa4291a00f7073082d8aff50b0630a
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 21:24:31 +0200

    Add "Types - string - Literals" example.

---
+++ 04 - Types/j. string/1 - Literals/Program.cs
@@ -0,0 +1,29 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+// Single line
+string str = "Text";
+Console.WriteLine(str);                              // Text
+Console.WriteLine(str.Length);                       // 4
+Console.WriteLine(str.Substring(1, 2));              // ex
+Console.WriteLine(str.GetType().FullName);           // System.String
+
+// Multi line
+string multi1 = @"
+    one
+    ""two""
+";
+Console.WriteLine(multi1);
+
+string multi2 = @"
+    one
+    ""two""
+".Trim();
+Console.WriteLine(multi2);
+
+// Get character
+Console.WriteLine(str[0]);                     // T
+Console.WriteLine(str[0].GetType().FullName);  // System.Char
+
+// Special characters (single line)
+Console.WriteLine("first \"line\" \nsecond line");
