2b73112ef8543a94cb6fb59bfc4ae0c7ab87ed14
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 21:33:05 +0200

    Add "Types - string - Interpolation" example.

---
+++ 04 - Types/j. string/2 - Interpolation/Program.cs
@@ -0,0 +1,24 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+bool boolean = true;
+int number = 123;
+string text = "abc";
+
+// Single line
+Console.WriteLine($"bool: {boolean}, number+1: {number + 1}, text: {text} (length: {text.Length}), slash: \\, double quotes: \"");
+// bool: true, number+1: 124, text: abc (length: 3), slash: \, double quotes: "
+
+// Multi line
+Console.WriteLine($@"
+bool: {boolean}
+number+1: {number + 1}
+text: {text} (length: {text.Length})
+slash: \
+double quotes: ""
+");
+// bool: true
+// number+1: 124
+// text: abc (length: 3)
+// slash: \
+// double quotes: "
