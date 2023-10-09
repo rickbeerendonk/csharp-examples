a7fd0588a819af4d94b83646d22a62743014812d
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 20:16:53 +0200

    Add "Types - Long" example.

---
+++ 04 - Types/e1. long/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(long.MinValue); // -9223372036854775808
+Console.WriteLine(long.MaxValue); // 9223372036854775807
+
+// Explicit vs Implicit
+long explicitValue = 9223372036854775807;
+var implicitValue = 9223372036854775807;
+
+Console.WriteLine(explicitValue.GetType().FullName); // System.Int64
+Console.WriteLine(implicitValue.GetType().FullName); // System.Int64
