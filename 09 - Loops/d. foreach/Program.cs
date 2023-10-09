2a11b8f1e34ed9bf1b7de8b4051014b5f7eef317
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 23:45:38 +0200

    Add "Loops - foreach" example.

---
+++ 09 - Loops/d. foreach/Program.cs
@@ -0,0 +1,14 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+string[] players = { "Rafael Nadal", "Roger Federer", "Serena Williams" };
+
+// One line
+foreach (var player in players)
+{
+  Console.WriteLine(player);
+}
+
+// Rafael Nadal
+// Roger Federer
+// Serena Williams
