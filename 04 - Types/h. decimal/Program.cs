dd6523010f7cde126c0c201f8443606e5ff552db
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 21:04:46 +0200

    Add "Types - decimal" example.

---
+++ 04 - Types/h. decimal/Program.cs
@@ -0,0 +1,14 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(decimal.MinValue); // -79228162514264337593543950335
+Console.WriteLine(decimal.MaxValue); // 79228162514264337593543950335
+
+// Explicit vs Implicit
+decimal explicitValue = 1.23M; // M suffix required
+var implicitValue1 = 1.23;
+var implicitValue2 = 1.23M;    // M suffix
+
+Console.WriteLine(explicitValue.GetType().FullName);  // System.Decimal
+Console.WriteLine(implicitValue1.GetType().FullName); // System.Double
+Console.WriteLine(implicitValue2.GetType().FullName); // System.Decimal
