daca86ba94a336641bce897b1cb9ae93fe89f680
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 23:44:28 +0200

    Add "Loops - for" example.

---
+++ 09 - Loops/c. for/Program.cs
@@ -0,0 +1,13 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+string[] players = { "Rafael Nadal", "Roger Federer", "Serena Williams" };
+
+for (var i = 0; i < players.Length; i++)
+{
+  Console.WriteLine(players[i]);
+}
+
+// Rafael Nadal
+// Roger Federer
+// Serena Williams
