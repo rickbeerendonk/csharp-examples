ca59801fdac5d1a82f2c827395a6b5e5944bc565
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 21:17:04 +0200

    Add "Types - char" example.

---
+++ 04 - Types/i. char/Program.cs
@@ -0,0 +1,17 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+char charValue = 'a';
+Console.WriteLine(charValue);                              // a
+Console.WriteLine(charValue.GetType().FullName);           // System.Char
+
+// Special
+Console.WriteLine('\t');                                   // (tab)
+Console.WriteLine('\b');                                   // (backspace)
+Console.WriteLine('\n');                                   // (new-line)
+Console.WriteLine('\r');                                   // (return)
+Console.WriteLine('\'');                                   // '
+Console.WriteLine('\"');                                   // "
+Console.WriteLine('\\');                                   // \
+Console.WriteLine('$');                                    // $
+Console.WriteLine('\uFF00');
