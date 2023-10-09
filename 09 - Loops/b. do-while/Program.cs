0ede7dd378adfc329089d11bffb8cc442dd16afc
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 23:42:05 +0200

    Add "Loops - do-while" example.

---
+++ 09 - Loops/b. do-while/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+var i = 10;
+
+do
+{
+  Console.WriteLine($"Iteration {i}");
+  i++;
+} while (i < 5);
+
+Console.WriteLine("End");
+
+// Iteration 10
+// End
