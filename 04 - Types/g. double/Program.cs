f68fe428891fbd7d5edc161238fd87b95af9ec07
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 21:00:12 +0200

    Add "Types - double" example.

---
+++ 04 - Types/g. double/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(double.MinValue); // -1.7976931348623157E+308
+Console.WriteLine(double.MaxValue); // 1.7976931348623157E+308
+
+// Explicit vs Implicit
+double explicitValue = 1.23; // F suffix required
+var implicitValue = 1.23;
+
+Console.WriteLine(explicitValue.GetType().FullName);  // System.Double
+Console.WriteLine(implicitValue.GetType().FullName); // System.Double
