65945850e923406a705e6baac33ed7d156fb9cdf
Rick Beerendonk <rick@beerendonk.com>
2023-10-09 20:27:59 +0200

    Add "Types - ULong" example.

---
+++ 04 - Types/e2. ulong/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+Console.WriteLine(ulong.MinValue); // 0
+Console.WriteLine(ulong.MaxValue); // 18446744073709551615
+
+// Explicit vs Implicit
+ulong explicitValue = 18446744073709551615;
+var implicitValue = 18446744073709551615;  // ulong
+
+Console.WriteLine(explicitValue.GetType().FullName); // System.UInt64
+Console.WriteLine(implicitValue.GetType().FullName); // System.UInt64
