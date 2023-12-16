136fc538ea47103e1fd8a79afe51917afe4bb345
Rick Beerendonk <rick@beerendonk.com>
2023-12-17 00:24:30 +0100

    Add "Conversions - toBoolean" demo.

---
+++ 05 - Conversions/a. toBoolean/Program.cs
@@ -0,0 +1,24 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+// Integers
+
+Console.WriteLine(Convert.ToBoolean(0));  // False
+Console.WriteLine(Convert.ToBoolean(1));  // True
+
+// Floating point
+
+Console.WriteLine(Convert.ToBoolean(0.0));  // False
+Console.WriteLine(Convert.ToBoolean(0.1));  // True
+
+// String
+
+Console.WriteLine(Convert.ToBoolean(null));     // False
+Console.WriteLine(Convert.ToBoolean("False"));  // False
+Console.WriteLine(Convert.ToBoolean("false"));  // False
+Console.WriteLine(Convert.ToBoolean("True"));   // True
+Console.WriteLine(Convert.ToBoolean("true"));   // True
+
+Console.WriteLine(Convert.ToBoolean("other"));  // System.FormatException: String 'other' was not recognized as a valid Boolean.
+
+
