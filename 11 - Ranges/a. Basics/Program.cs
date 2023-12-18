1d3b2dca397379e1a8c03631090d7010626c165f
Rick Beerendonk <rick@beerendonk.com>
2023-12-19 00:25:15 +0100

    Add "Ranges - Basics" demo.

---
+++ 11 - Ranges/a. Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+int[] values = [1, 2, 3, 4, 5, 6, 7];
+
+Console.WriteLine(string.Join(", ", values[..]));     // 1, 2, 3, 4, 5, 6, 7
+Console.WriteLine(string.Join(", ", values[2..]));    //       3, 4, 5, 6, 7
+Console.WriteLine(string.Join(", ", values[..2]));    // 1, 2
+Console.WriteLine(string.Join(", ", values[..^2]));   // 1, 2, 3, 4, 5
+Console.WriteLine(string.Join(", ", values[^2..]));   //             6, 7
+Console.WriteLine(string.Join(", ", values[2..^2]));  //       3, 4, 5
+Console.WriteLine(string.Join(", ", values[^5..5]));  //       3, 4, 5
