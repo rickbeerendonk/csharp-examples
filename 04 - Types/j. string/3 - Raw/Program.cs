43e0b9372ab4db7909110f82ba66985ca1a831bc
Rick Beerendonk <rick@beerendonk.com>
2024-01-01 00:25:37 +0100

    Add "Types - string - Raw" demo.

---
+++ 04 - Types/j. string/3 - Raw/Program.cs
@@ -0,0 +1,14 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+// Raw String
+Console.WriteLine("""
+  "Line1"\n"Line2"
+  """);
+// "Line1"\n"Line2"
+
+// Raw String with Interpolation
+Console.WriteLine($"""
+  "Line{1}"\n"Line{1+1}"
+  """);
+// "Line1"\n"Line2"
