f1250b2934326fa9eaf2487e5b7e2dc9b818702e
Rick Beerendonk <rick@beerendonk.com>
2024-01-06 00:27:35 +0100

    Use Collection Expression.

--- 09 - Loops/c. for/Program.cs
+++ 09 - Loops/c. for/Program.cs
@@ -1,7 +1,7 @@
 ﻿// European Union Public License version 1.2
 // Copyright © 2023 Rick Beerendonk
 
-string[] players = { "Rafael Nadal", "Roger Federer", "Serena Williams" };
+string[] players = ["Rafael Nadal", "Roger Federer", "Serena Williams"];
 
 for (var i = 0; i < players.Length; i++)
 {
