55104e6f924ff8652ed6a02a7ac6e4675d7a1fc6
Rick Beerendonk <rick@beerendonk.com>
2024-01-07 00:40:35 +0100

    Add "Loops - Break and Continue" example.

---
+++ 09 - Loops/e. break and continue/Program.cs
@@ -0,0 +1,16 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+for (var i = 0; i < 20; i++)
+{
+  if (i % 3 == 0) continue;
+  if (i % 10 == 0) break;
+  Console.WriteLine(i);
+}
+
+// 1
+// 2
+// 4
+// 5
+// 7
+// 8
