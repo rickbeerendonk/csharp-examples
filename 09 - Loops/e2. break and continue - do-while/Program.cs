9b4121211c5313e223d1ca99d654dadcdc38a40d
Rick Beerendonk <rick@beerendonk.com>
2024-01-09 01:03:50 +0100

    Add "Loops - Break and Continue - Do-While" example.

---
+++ 09 - Loops/e2. break and continue - do-while/Program.cs
@@ -0,0 +1,19 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+int i = 0;
+
+do
+{
+  i++;
+  if (i % 3 == 0) continue;
+  if (i % 10 == 0) break;
+  Console.WriteLine(i);
+} while (i < 20);
+
+// 1
+// 2
+// 4
+// 5
+// 7
+// 8
