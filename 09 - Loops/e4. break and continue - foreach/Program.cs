d7aa71924f8036611b12d288507d86dfc48b541b
Rick Beerendonk <rick@beerendonk.com>
2024-01-10 01:04:02 +0100

    Add "Loops - Break and Continue - ForEach" example.

---
+++ 09 - Loops/e4. break and continue - foreach/Program.cs
@@ -0,0 +1,19 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+                     11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+foreach (var n in numbers)
+{
+  if (n % 3 == 0) continue;
+  if (n % 10 == 0) break;
+  Console.WriteLine(n);
+}
+
+// 1
+// 2
+// 4
+// 5
+// 7
+// 8
