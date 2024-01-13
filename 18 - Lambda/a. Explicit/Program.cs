f9e62595f8de5567dcf878082a53ac4430b4f0cf
Rick Beerendonk <rick@beerendonk.com>
2024-01-13 02:03:11 +0100

    Add "Lambda - Explicit" example.

---
+++ 18 - Lambda/a. Explicit/Program.cs
@@ -0,0 +1,18 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+var a = () => "EcmaScript";
+
+var b = (int x) => x * x;
+var d = (int x) => 
+{
+    int y = x * x;
+    return y;
+}; // Add { ...; return ...; }
+
+var e = (int x, int y) => x * y;
+
+Console.WriteLine(a());     // EcmaScript
+Console.WriteLine(b(2));    // 4
+Console.WriteLine(d(4));    // 16
+Console.WriteLine(e(5, 6)); // 30
