feb39858c74674fdcd5528074df0f065647413a4
Rick Beerendonk <rick@beerendonk.com>
2024-01-17 09:37:55 +0100

    Add "Classes - Constructors - Two" example.

---
+++ 13 - Classes/04 - Constructors/c. Two/Program.cs
@@ -0,0 +1,19 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+var demo = new Demo("one");
+
+Console.WriteLine(demo.Name);
+
+// Constructor done
+// one
+
+
+class Demo {
+  public Demo(string name) {
+    Name = name;
+    Console.WriteLine("Constructor done");
+  }
+
+  public string Name { get; }
+}
