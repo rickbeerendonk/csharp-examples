cecb23289c19f39200b317681dd0949c4b3a90dc
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 19:52:30 +0100

    Add "LINQ - Filtering - OfType" example.

---
+++ 25 - LINQ/01 - Filtering/b. OfType/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<object> items = [1, "two", 3, true, 5];
+
+var numbers = items.OfType<int>();
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+// 1
+// 3
+// 5
