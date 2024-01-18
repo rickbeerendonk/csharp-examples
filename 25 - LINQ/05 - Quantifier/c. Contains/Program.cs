be941484e79088b8b112b7a5663e80a3229be1fb
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 23:10:25 +0100

    Add TODO.

---
+++ 25 - LINQ/05 - Quantifier/c. Contains/Program.cs
@@ -0,0 +1,10 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+IList<int> numbers = new List<int> { 1, 3, 5, 7, 9 };
+
+Console.WriteLine(numbers.Contains(7));  // True
+
+numbers[3] = 6;
+
+Console.WriteLine(numbers.Contains(7));  // False
