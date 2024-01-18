6622c9a4df66da94e94ae6a77786dbce32c69d79
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 20:34:30 +0100

    Add "LINQ - Projection - SelectMany" example.

---
+++ 25 - LINQ/02 - Projection/b. SelectMany/Program.cs
@@ -0,0 +1,22 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<List<string>> citiesPerCountry = [
+    ["Amsterdam", "Rotterdam"],
+    ["Oslo", "Bergen"],
+    ["Copenhagen", "Aarhus"]
+];
+
+var cities = citiesPerCountry.SelectMany(c => c);
+
+foreach (var city in cities)
+{
+    Console.WriteLine(city);
+}
+
+// Amsterdam
+// Rotterdam
+// Oslo
+// Bergen
+// Copenhagen
+// Aarhus
