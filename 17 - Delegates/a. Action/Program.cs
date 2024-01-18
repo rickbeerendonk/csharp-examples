1e0642308d7bae9bdb87bd50a46f122b09694a12
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 13:33:46 +0100

    Add "Delegates - Action" example.

---
+++ 17 - Delegates/a. Action/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+// Action
+// Action<in>
+// Action<in1, in2>
+
+Action<string> bar = Foo.Bar;
+bar("abc");  // Foo.Bar("abc")
+
+class Foo
+{
+    public static void Bar(string value)
+        => Console.WriteLine($"Foo.Bar({value})");
+}
