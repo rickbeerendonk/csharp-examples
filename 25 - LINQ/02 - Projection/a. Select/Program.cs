1dfb6b82e033dc37b811f4c00a5a89b315b6842b
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 19:59:22 +0100

    Add "LINQ - Projection - Select" example.

---
+++ 25 - LINQ/02 - Projection/a. Select/Program.cs
@@ -0,0 +1,16 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<string> names = ["Alex Allen", "Ben Berends", "Chris Cole", "David Davenport"];
+
+var firstNames = names.Select(n => n.Split(' ').First());
+
+foreach (var firstName in firstNames)
+{
+    Console.WriteLine(firstName);
+}
+
+// Alex
+// Ben
+// Chris
+// David
