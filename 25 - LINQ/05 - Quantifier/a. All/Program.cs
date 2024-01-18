0e3a4ee226cea19458b4e59093fdf9bb35829fc5
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 22:01:53 +0100

    Add "LINQ - Quantifier - All" example.

---
+++ 25 - LINQ/05 - Quantifier/a. All/Program.cs
@@ -0,0 +1,10 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<int> numbers = [1, 3, 5, 7, 9];
+
+Console.WriteLine(numbers.All(n => n % 2 == 1));  // True
+
+numbers[4] = 6;
+
+Console.WriteLine(numbers.All(n => n % 2 == 1));  // False
