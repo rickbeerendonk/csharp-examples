b613e1a01ecc96cbb7e2768ddbd9f8f887ae1e0e
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 09:53:33 +0100

    Add "Classes - Inheritance - Inherit All" example.

---
+++ 13 - Classes/06 - Inheritance/a. Inherit All/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+MySuper sup = new();
+sup.Foo(); // MySuper.Foo
+
+MySub sub = new();
+sub.Foo(); // MySuper.Foo
+
+class MySuper
+{
+  public void Foo() => Console.WriteLine("MySuper.Foo");
+}
+
+class MySub : MySuper {}
