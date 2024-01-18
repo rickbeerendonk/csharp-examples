fd44b66050164b3dfc8ef05b2270611cef84bcd7
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 13:33:52 +0100

    Add "Delegates - Func" example.

---
+++ 17 - Delegates/b. Func/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+// Func<out>
+// Func<in, out>
+// Func<in1, in2, out>
+
+Func<string, int> bar = Foo.Bar;
+Console.WriteLine(bar("abc"));  // 3
+
+class Foo
+{
+    public static int Bar(string value)
+        => value.Length;
+}
