bbd093718362687ccf2c7cfee3807bfa52a9e783
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 21:08:20 +0100

    Add "LINQ - Projection - Zip with Result Selector" example.

---
+++ 25 - LINQ/02 - Projection/c2. Zip with Result Selector/Program.cs
@@ -0,0 +1,17 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<string> firstNames = ["Alex", "Ben", "Chris", "David"];
+List<string> lastNames = ["Allen", "Berends", "Cole", "Davenport"];
+
+var names = firstNames.Zip(lastNames, (first, last) => $"{first} {last}");
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+// Alex Allen
+// Ben Berends
+// Chris Cole
+// David Davenport
