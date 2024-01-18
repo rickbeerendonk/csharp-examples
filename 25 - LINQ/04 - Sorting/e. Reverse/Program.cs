52783c734e7e0232cdbe5caa69a28455fea02f9b
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 21:33:38 +0100

    Add "LINQ - Sorting - Reverse" example.

---
+++ 25 - LINQ/04 - Sorting/e. Reverse/Program.cs
@@ -0,0 +1,17 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+IEnumerable<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+var reverseNumbers = numbers.Reverse();
+
+foreach (var number in reverseNumbers)
+{
+    Console.WriteLine(number);
+}
+
+// 5
+// 4
+// 3
+// 2
+// 1
