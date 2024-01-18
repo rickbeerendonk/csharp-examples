5275c69653a68751ee8a31eaae4d613c98f2ee88
Rick Beerendonk <rick@beerendonk.com>
2024-01-19 00:08:53 +0100

    Add "LINQ - Concatenation - Concat" example.

---
+++ 25 - LINQ/11 - Concatenation/a. Concat/Program.cs
@@ -0,0 +1,18 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<int> numbers1 = [1, 2, 3];
+List<int> numbers2 = [4, 5];
+
+var numbers = numbers1.Concat(numbers2);
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+// 1
+// 2
+// 3
+// 4
+// 5
