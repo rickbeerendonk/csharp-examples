9341b4728715f2da94dad02faaf5a2e5d2b6d088
Rick Beerendonk <rick@beerendonk.com>
2024-01-18 19:45:10 +0100

    Add "LINQ - Filtering - Where" example.

---
+++ 25 - LINQ/01 - Filtering/a. Where/Program.cs
@@ -0,0 +1,14 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<int> numbers = [1, 2, 3, 4, 5];
+
+var evenNumbers = numbers.Where(n => n % 2 == 0);
+
+foreach (var number in evenNumbers)
+{
+    Console.WriteLine(number);
+}
+
+// 2
+// 4
