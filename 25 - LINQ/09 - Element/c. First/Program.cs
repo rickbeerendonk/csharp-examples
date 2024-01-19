a521e0ff20ec2d9b585cbde0522e3a524a52c784
Rick Beerendonk <rick@beerendonk.com>
2024-01-19 09:00:32 +0100

    Add "LINQ - Element - First" example.

---
+++ 25 - LINQ/09 - Element/c. First/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<int> numbers = [1, 2, 3];
+
+// Without filter
+Console.WriteLine(numbers.First());  // 1
+
+// With filter
+Console.WriteLine(numbers.First(n => n % 2 == 0));  // 2
+
+numbers.Clear();
+
+// Empty collection
+Console.WriteLine(numbers.First());  // InvalidOperationException
