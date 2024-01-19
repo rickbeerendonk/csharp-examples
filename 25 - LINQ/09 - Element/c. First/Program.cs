f013c8456dcbccebe13eeac9b19d3f205718c83d
Rick Beerendonk <rick@beerendonk.com>
2024-01-19 10:02:08 +0100

    Small improvements.

--- 25 - LINQ/09 - Element/c. First/Program.cs
+++ 25 - LINQ/09 - Element/c. First/Program.cs
@@ -1,7 +1,7 @@
 ﻿// European Union Public License version 1.2
 // Copyright © 2024 Rick Beerendonk
 
-List<int> numbers = [1, 2, 3];
+List<int> numbers = [1, 2, 3, 4, 5];
 
 // Without filter
 Console.WriteLine(numbers.First());  // 1
@@ -9,7 +9,7 @@ Console.WriteLine(numbers.First());  // 1
 // With filter
 Console.WriteLine(numbers.First(n => n % 2 == 0));  // 2
 
-numbers.Clear();
+numbers = [];
 
 // Empty collection
 Console.WriteLine(numbers.First());  // InvalidOperationException
--- 25 - LINQ/09 - Element/d. FirstOrDefault/Program.cs
+++ 25 - LINQ/09 - Element/d. FirstOrDefault/Program.cs
@@ -1,7 +1,7 @@
 ﻿// European Union Public License version 1.2
 // Copyright © 2024 Rick Beerendonk
 
-List<int> numbers = [1, 2, 3];
+List<int> numbers = [1, 2, 3, 4, 5];
 
 // Without filter
 Console.WriteLine(numbers.FirstOrDefault());  // 1
@@ -9,7 +9,7 @@ Console.WriteLine(numbers.FirstOrDefault());  // 1
 // With filter
 Console.WriteLine(numbers.FirstOrDefault(n => n % 2 == 0));  // 2
 
-numbers.Clear();
+numbers = [];
 
 // Empty collection
 Console.WriteLine(numbers.FirstOrDefault());  // 0
