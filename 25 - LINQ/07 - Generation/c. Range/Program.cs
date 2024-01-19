9947cc9ab95ed597f68370801b0a2bb660ef6b29
Rick Beerendonk <rick@beerendonk.com>
2024-01-19 08:55:04 +0100

    Add "LINQ - Generation - Range" example.

---
+++ 25 - LINQ/07 - Generation/c. Range/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+var evenNumbers = Enumerable.Range(1, 5);
+
+foreach (var number in evenNumbers)
+{
+    Console.WriteLine(number);
+}
+
+// 1
+// 2
+// 3
+// 4
+// 5
