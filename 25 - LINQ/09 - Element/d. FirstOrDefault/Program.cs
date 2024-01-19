109b5e6310a4895f539af844ac07cea7ada72fd2
Rick Beerendonk <rick@beerendonk.com>
2024-01-19 09:01:12 +0100

    Add "LINQ - Element - FirstOrDefault" example.

---
+++ 25 - LINQ/09 - Element/d. FirstOrDefault/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<int> numbers = [1, 2, 3];
+
+// Without filter
+Console.WriteLine(numbers.FirstOrDefault());  // 1
+
+// With filter
+Console.WriteLine(numbers.FirstOrDefault(n => n % 2 == 0));  // 2
+
+numbers.Clear();
+
+// Empty collection
+Console.WriteLine(numbers.FirstOrDefault());  // 0
