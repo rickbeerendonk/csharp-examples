eeebb83095c13a21f8748b76a336ea75eaab0505
Rick Beerendonk <rick@beerendonk.com>
2024-01-19 18:21:53 +0100

    Add "LINQ - Element - Last" example.

---
+++ 25 - LINQ/09 - Element/e. Last/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<int> numbers = [1, 2, 3, 4, 5];
+
+// Without filter
+Console.WriteLine(numbers.Last());  // 5
+
+// With filter
+Console.WriteLine(numbers.Last(n => n % 2 == 0));  // 4
+
+numbers = [];
+
+// Empty collection
+Console.WriteLine(numbers.Last());  // InvalidOperationException
