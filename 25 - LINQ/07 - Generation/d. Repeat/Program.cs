afb8488ceee21ffcd571b4b3850044d396a8d32a
Rick Beerendonk <rick@beerendonk.com>
2024-01-19 10:01:43 +0100

    Add "LINQ - Generation - Repeat" example.

---
+++ 25 - LINQ/07 - Generation/d. Repeat/Program.cs
@@ -0,0 +1,13 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+var numbers = Enumerable.Repeat(1, 3);
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+// 1
+// 1
+// 1
