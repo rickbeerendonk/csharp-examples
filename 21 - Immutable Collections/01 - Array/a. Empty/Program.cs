fb25a3e45e3954e1214c2db33af57c262e68b2af
Rick Beerendonk <rick@beerendonk.com>
2024-02-16 22:38:34 +0100

    Add "Immutable Collections - Array - Empty" example.

---
+++ 21 - Immutable Collections/01 - Array/a. Empty/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+var arr = ImmutableArray<int>.Empty;
+
+Console.WriteLine(arr.Length); // 0
