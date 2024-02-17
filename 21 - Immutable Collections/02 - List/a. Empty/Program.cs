676d1888121e869ebd2dc76a75403c0026ca39c0
Rick Beerendonk <rick@beerendonk.com>
2024-02-17 22:42:35 +0100

    Add "Immutable Collections - List - Empty" example.

---
+++ 21 - Immutable Collections/02 - List/a. Empty/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+var lst = ImmutableList<int>.Empty;
+
+Console.WriteLine(lst.Count); // 0
