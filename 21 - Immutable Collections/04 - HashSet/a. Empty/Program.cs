eb10ebf6e122ae1cc520809d131179374648c79e
Rick Beerendonk <rick@beerendonk.com>
2024-02-20 23:15:36 +0100

    Add "Immutable Collections - HashSet - Empty" example.

---
+++ 21 - Immutable Collections/04 - HashSet/a. Empty/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+var set = ImmutableHashSet<int>.Empty;
+
+Console.WriteLine(set.Count); // 0
