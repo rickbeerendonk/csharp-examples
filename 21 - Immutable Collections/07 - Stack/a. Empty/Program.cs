e3878d834a8735dd8f29833cbacb495c27186bde
Rick Beerendonk <rick@beerendonk.com>
2024-02-23 23:16:50 +0100

    Add "Immutable Collections - Stack - Empty" example.

---
+++ 21 - Immutable Collections/07 - Stack/a. Empty/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+var stack = ImmutableStack<int>.Empty;
+
+Console.WriteLine(stack.IsEmpty); // True
