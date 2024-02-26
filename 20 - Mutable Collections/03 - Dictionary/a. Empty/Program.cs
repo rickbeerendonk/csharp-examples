2b97ffb81e3ac69aeeb3045a4f8aa4fe539495e8
Rick Beerendonk <rick@beerendonk.com>
2024-02-26 23:56:23 +0100

    Add "Mutable Collections - Dictionary - Empty" example.

---
+++ 20 - Mutable Collections/03 - Dictionary/a. Empty/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+using System.Collections;
+
+var dic = new Dictionary<string, int>();
+
+Console.WriteLine(dic.Count); // 0
