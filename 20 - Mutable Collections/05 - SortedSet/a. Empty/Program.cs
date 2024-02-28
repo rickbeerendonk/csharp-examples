df512e6a84cccb2382182609e6875a51b15958c4
Rick Beerendonk <rick@beerendonk.com>
2024-02-28 23:57:05 +0100

    Add "Mutable Collections - SortedSet - Empty" example.

---
+++ 20 - Mutable Collections/05 - SortedSet/a. Empty/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+using System.Collections;
+
+var set = new SortedSet<int>();
+
+Console.WriteLine(set.Count); // 0
