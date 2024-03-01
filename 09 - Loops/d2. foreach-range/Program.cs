afc581e35e14a33dc4d7cee5da44ce7330070355
Rick Beerendonk <rick@beerendonk.com>
2024-03-02 00:22:07 +0100

    Add "Loops - ForEach - Range" example.

---
+++ 09 - Loops/d2. foreach-range/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+foreach (var i in Enumerable.Range(2, 4))
+{
+  Console.WriteLine(i);
+}
+
+// 2
+// 3
+// 4
+// 5
