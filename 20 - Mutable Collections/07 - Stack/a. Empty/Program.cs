8a67f5f40621f9e725d4effc2b637947b633b269
Rick Beerendonk <rick@beerendonk.com>
2024-03-01 23:57:47 +0100

    Add "Mutable Collections - Stack - Empty" example.

---
+++ 20 - Mutable Collections/07 - Stack/a. Empty/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+using System.Collections;
+
+var stack = new Stack<int>();
+
+Console.WriteLine(stack.Count); // 0
