48f96c81da09f8084402b9dbbe3e1428d5e28235
Rick Beerendonk <rick@beerendonk.com>
2024-03-10 15:24:39 +0100

    Remove unnecessary using.

--- 20 - Mutable Collections/03 - Dictionary/a. Empty/Program.cs
+++ 20 - Mutable Collections/03 - Dictionary/a. Empty/Program.cs
@@ -1,8 +1,6 @@
 ﻿// European Union Public License version 1.2
 // Copyright © 2024 Rick Beerendonk
 
-using System.Collections;
-
 var dic = new Dictionary<string, int>();
 
 Console.WriteLine(dic.Count); // 0
