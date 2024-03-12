0194bf9eb6f7081fec46781633268302845a881b
Rick Beerendonk <rick@beerendonk.com>
2024-03-12 15:25:13 +0100

    Add "Mutable Collections - Array - Create - Default" example.

---
+++ 20 - Mutable Collections/01 - Array/b. Create - i. Default/Program.cs
@@ -0,0 +1,9 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+// Collection expression
+var arr = new int[2];
+
+Console.WriteLine(arr.Length); // 3
+Console.WriteLine(arr[0]); // 0
+Console.WriteLine(arr[1]); // 0
