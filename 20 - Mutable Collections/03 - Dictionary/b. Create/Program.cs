2785273f8f6009a9881f9b74814dcb98eafee023
Rick Beerendonk <rick@beerendonk.com>
2024-03-23 15:26:10 +0100

    Add "Mutable Collections - Dictionary - Create" example.

---
+++ 20 - Mutable Collections/03 - Dictionary/b. Create/Program.cs
@@ -0,0 +1,6 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+var dic = new Dictionary<string, int> { { "one", 1 }, { "two", 2 } };
+
+Console.WriteLine(dic.Count); // 2
