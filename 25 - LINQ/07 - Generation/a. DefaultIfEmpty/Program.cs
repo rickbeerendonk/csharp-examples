bf85935a618744851149749439a214b0342ee31e
Rick Beerendonk <rick@beerendonk.com>
2025-01-31 22:09:33 +0100

    Add "LINQ - Generation - DefaultIfEmpty" example.

---
+++ 25 - LINQ/07 - Generation/a. DefaultIfEmpty/Program.cs
@@ -0,0 +1,34 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+List<int> numbers = new();
+
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("---");
+// ---
+
+foreach (int number in numbers.DefaultIfEmpty())
+{
+    Console.WriteLine(number);
+}
+
+// 0
+
+Console.WriteLine("---");
+// ---
+
+numbers.Add(1);
+numbers.Add(2);
+
+foreach (int number in numbers.DefaultIfEmpty())
+{
+    Console.WriteLine(number);
+}
+
+// 1
+// 2
+
