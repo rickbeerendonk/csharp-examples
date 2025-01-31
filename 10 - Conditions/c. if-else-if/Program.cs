d70068d31a713a50267dab84432237af79ff2255
Rick Beerendonk <rick@beerendonk.com>
2025-01-31 19:17:57 +0100

    Add "Conditions - if-else-if" example.

---
+++ 10 - Conditions/c. if-else-if/Program.cs
@@ -0,0 +1,22 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+var moment = DateTime.Now;
+var hour = moment.Hour;
+
+if (hour < 6)
+{
+    Console.WriteLine("Good night");
+}
+else if (hour < 12)
+{
+    Console.WriteLine("Good morning");
+}
+else if (hour < 18)
+{
+    Console.WriteLine("Good afternoon");
+}
+else
+{
+    Console.WriteLine("Good evening");
+}
