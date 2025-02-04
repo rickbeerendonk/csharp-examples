bd32cc2f84098d684665fa5a8ba6bb038fe9b22c
Rick Beerendonk <rick@beerendonk.com>
2025-02-04 08:53:41 +0100

    Add "Function With Return" example.

---
+++ 14 - Functions [TODO]/b. FunctionWithReturn/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+Console.WriteLine(TestString());
+Console.WriteLine(TestInt());
+
+string TestString()
+{
+  return "Result";
+}
+
+int TestInt()
+{
+  return 123;
+}
