f30747df84f7d39ce113b039ccec8f83ec1a78ac
Rick Beerendonk <rick@beerendonk.com>
2025-02-04 09:08:29 +0100

    Add "Functions - Optional Params" example.

---
+++ 14 - Functions [TODO]/d. Optional Params/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+Test("Hello C#", 12);
+
+void Test(string text, int number = 1, bool boolean = true)
+{
+  Console.WriteLine(text);
+  Console.WriteLine(number);
+  Console.WriteLine(boolean);
+}
+
+// Hello C#
+// 12
+// True
