bf7fe03bd89ad503b5c08b00f6da4031d16f4488
Rick Beerendonk <rick@beerendonk.com>
2025-02-04 09:13:58 +0100

    Add "Functions with Params Parameter" example.

---
+++ 14 - Functions [TODO]/e. Params/Program.cs
@@ -0,0 +1,19 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+Test("Hello C#", 1, 2, 3, 4);
+
+void Test(string text, params int[] numbers)
+{
+  Console.WriteLine(text);
+  foreach (var number in numbers)
+  {
+    Console.WriteLine(number);
+  }
+}
+
+// Hello C#
+// 1
+// 2
+// 3
+// 4
