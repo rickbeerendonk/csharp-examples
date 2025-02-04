31c20ccced637a80a5c4adfc2d6093361217af2d
Rick Beerendonk <rick@beerendonk.com>
2025-02-04 09:01:26 +0100

    Add "Function with Return" example.

---
+++ 14 - Functions [TODO]/c. FunctionWithParams/Program.cs
@@ -0,0 +1,10 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+Test("Hello C#", 12);
+
+void Test(string text, int number)
+{
+  Console.WriteLine(text);
+  Console.WriteLine(number);
+}
