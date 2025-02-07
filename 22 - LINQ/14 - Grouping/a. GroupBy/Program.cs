9c6acef36d5f587da6d9ffea6ab05cb378a73edd
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 12:52:42 +0100

    Add "LINQ - Grouping - GroupBy" example.

---
+++ 22 - LINQ/14 - Grouping/a. GroupBy/Program.cs
@@ -0,0 +1,36 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+IEnumerable<Person> persons = [
+    new Person("Alexandra", 34),
+    new Person("Bob", 22),
+    new Person("Charlie", 51),
+    new Person("David", 22),
+    new Person("Eva", 34)
+    ];
+
+var personGroups = persons.GroupBy(p => p.Age);
+
+foreach (var group in personGroups)
+{
+    Console.WriteLine($"Age: {group.Key}");
+
+    foreach (var person in group)
+    {
+        Console.WriteLine($"- {person}");
+    }
+}
+
+record Person(string Name, int Age);
+
+// Age: 34
+// - Person { Name = Alexandra, Age = 34 }
+// - Person { Name = Eva, Age = 34 }
+// Age: 22
+// - Person { Name = Bob, Age = 22 }
+// - Person { Name = David, Age = 22 }
+// Age: 51
+// - Person { Name = Charlie, Age = 51 }
