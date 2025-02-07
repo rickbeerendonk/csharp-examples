b1b9b85df40b3eca4c0d9be9fdd8af3d127d9b67
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 02:54:27 +0100

    Add "LINQ - Quantifier - Any" example.

---
+++ 25 - LINQ/05 - Quantifier/b. Any/Program.cs
@@ -0,0 +1,12 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+List<int> numbers = [1, 3, 5, 7, 9];
+
+// Any even number?
+Console.WriteLine(numbers.Any(n => n % 2 == 0));  // False
+
+numbers[3] = 6;
+
+// Any even number?
+Console.WriteLine(numbers.Any(n => n % 2 == 0));  // True
