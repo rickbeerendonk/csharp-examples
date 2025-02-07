f20ca6c9466c719f3ef1293b2cea764590053c99
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 10:45:02 +0100

    Add "LINQ - Sorting - OrderByDescending" example.

---
+++ 22 - LINQ/04 - Sorting/b. OrderByDescending/Program.cs
@@ -0,0 +1,28 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+IEnumerable<Person> persons = [
+    new Person("Alexandra", 34),
+    new Person("Bob", 22),
+    new Person("Charlie", 51),
+    new Person("David", 22),
+    new Person("Eva", 34)
+    ];
+
+var sortedPersons = persons.OrderByDescending(p => p.Age);
+
+foreach (var person in sortedPersons)
+{
+    Console.WriteLine(person);
+}
+
+record Person(string Name, int Age);
+
+// Person { Name = Charlie, Age = 51 }
+// Person { Name = Alexandra, Age = 34 }
+// Person { Name = Eva, Age = 34 }
+// Person { Name = Bob, Age = 22 }
+// Person { Name = David, Age = 22 }
