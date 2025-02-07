49fa1e0597b72fc3d68bf7614ec5e1f717790082
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 11:07:38 +0100

    Add "LINQ - Element - Single" example.

---
+++ 22 - LINQ/09 - Element/g. Single/Program.cs
@@ -0,0 +1,19 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+List<int> numbers = [123];
+
+// Without filter
+Console.WriteLine(numbers.Single());  // 123
+
+numbers = [1, 2, 3];
+
+// With filter
+Console.WriteLine(numbers.Single(n => n % 2 == 0));  // 2
+
+numbers = [];
+// numbers = [1, 2]
+
+// Empty collection
+//   Throws when there is not exactly one element.
+Console.WriteLine(numbers.Single());  // InvalidOperationException
