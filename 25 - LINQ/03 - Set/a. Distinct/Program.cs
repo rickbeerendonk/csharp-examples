8ac613ac0c0c56266f4b26ccf548c01396bcf5e8
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 02:43:04 +0100

    Add "LINQ - Set - Distinct" example.

---
+++ 25 - LINQ/03 - Set/a. Distinct/Program.cs
@@ -0,0 +1,16 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+IEnumerable<int> numbers = [1, 3, 1, 4, 2, 3, 2, 4];
+
+var distinctNumbers = numbers.Distinct();
+
+foreach (var number in distinctNumbers)
+{
+    Console.WriteLine(number);
+}
+
+// 1
+// 3
+// 4
+// 2
