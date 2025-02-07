bddfdccfb66749e7b768dd2991061a46a2bf9a46
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 10:55:31 +0100

    Add "LINQ - Partitioning - Skip" example.

---
+++ 22 - LINQ/06 - Partitioning/a. Skip/Program.cs
@@ -0,0 +1,16 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+IEnumerable<string> names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+
+var namesResult = names.Skip(2);
+
+foreach (var name in namesResult)
+{
+    Console.WriteLine(name);
+}
+
+// Charlie
+// David
+// Eve
+// Frank
