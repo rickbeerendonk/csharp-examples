168406202f38b66ec60f81b0915d0f7027855e88
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 03:45:16 +0100

    Add "Extension function" example.

---
+++ 24 - Extension Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+var demo = new Demo();
+
+// Use extension method
+Console.WriteLine(demo.Sum());  // 3
+
+class Demo
+{
+    public int First { get; } = 1;
+    public int Second { get; } = 2;
+}
+
+
+// Extension method
+static class DemoExtensions
+{
+    public static int Sum(this Demo demo)
+    {
+        return demo.First + demo.Second;
+    }
+}
