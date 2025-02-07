826faa9dc6ff739f657a2c400a2c98881075cd85
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 10:58:44 +0100

    Add "LINQ - Partitioning - Take" example.

---
+++ 22 - LINQ/06 - Partitioning/c. Take/Program.cs
@@ -0,0 +1,14 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+IEnumerable<string> names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+
+var namesResult = names.Take(2);
+
+foreach (var name in namesResult)
+{
+    Console.WriteLine(name);
+}
+
+// Alice
+// Bob
