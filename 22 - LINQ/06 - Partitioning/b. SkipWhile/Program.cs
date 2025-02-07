f61a4a6340be1a64ec2bebc09cf522e83959fa17
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 10:57:12 +0100

    Add "LINQ - Partitioning - SkipWhile" example.

---
+++ 22 - LINQ/06 - Partitioning/b. SkipWhile/Program.cs
@@ -0,0 +1,16 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+IEnumerable<string> names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+
+var namesResult = names.SkipWhile(name => name.Length <= 5);
+
+foreach (var name in namesResult)
+{
+    Console.WriteLine(name);
+}
+
+// Charlie
+// David
+// Eve
+// Frank
