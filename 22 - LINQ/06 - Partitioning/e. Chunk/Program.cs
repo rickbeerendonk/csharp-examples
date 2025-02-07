2401ddf03b25783abf65798e66c468e3f719a166
Rick Beerendonk <rick@beerendonk.com>
2025-02-07 11:01:22 +0100

    Add "LINQ - Partitioning - Chunk" example.

---
+++ 22 - LINQ/06 - Partitioning/e. Chunk/Program.cs
@@ -0,0 +1,15 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+IEnumerable<string> names = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+
+var result = names.Chunk(2);
+
+foreach (var chunk in result)
+{
+    Console.WriteLine(string.Join(", ", chunk));
+}
+
+// Alice, Bob
+// Charlie, David
+// Eve, Frank
