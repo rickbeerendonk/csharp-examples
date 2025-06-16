a960f6bfaed0462a5c0f962531cc1bab494c71da
Rick Beerendonk <rick@beerendonk.com>
2025-06-16 22:35:55 +0200

    Use list and explicit type.

--- 09 - Loops/d1. foreach/Program.cs
+++ 09 - Loops/d1. foreach/Program.cs
@@ -1,10 +1,10 @@
 ﻿// European Union Public License version 1.2
 // Copyright © 2023 Rick Beerendonk
 
-string[] players = ["Rafael Nadal", "Roger Federer", "Serena Williams"];
+List<string> players = ["Rafael Nadal", "Roger Federer", "Serena Williams"];
 
 // One line
-foreach (var player in players)
+foreach (string player in players)
 {
   Console.WriteLine(player);
 }
