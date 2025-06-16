4808406ea00cb5ab1ba3daab2db0dbdb3b4e66ef
Rick Beerendonk <rick@beerendonk.com>
2025-06-16 09:32:32 +0200

    Add "Mutable Collections - List - Add" example.

---
+++ 20 - Mutable Collections/02 - List/d. Add/Program.cs
@@ -0,0 +1,19 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+// Collection expression
+List<string> lst = ["A", "B", "C"];
+
+lst.Add("D");
+lst.Insert(1, "E"); // Inserts "E" at index 1
+
+foreach (string item in lst)
+{
+  Console.WriteLine(item);
+}
+
+// A
+// E
+// B
+// C
+// D
