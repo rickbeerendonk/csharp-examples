837987e9457aee3e53d537d40987f956b8a9d39e
Rick Beerendonk <rick@beerendonk.com>
2025-06-16 09:33:39 +0200

    Add "Mutable Collections - List - Delete" example.

---
+++ 20 - Mutable Collections/02 - List/e. Delete/Program.cs
@@ -0,0 +1,17 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+// Collection expression
+List<string> lst = ["A", "B", "C", "D", "E"];
+
+lst.RemoveAt(1); // Removes the second item
+lst.Remove("D"); // Removes "D"
+
+foreach (string item in lst)
+{
+  Console.WriteLine(item);
+}
+
+// A
+// C
+// E
