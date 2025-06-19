edd34afac01b4f6c151cd5e07a5ca905fcaacf0a
Rick Beerendonk <rick@beerendonk.com>
2025-06-19 10:16:47 +0200

    Add "Delegates - Predicate" example.

---
+++ 17 - Delegates/c. Predicate/Program.cs
@@ -0,0 +1,13 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+// bool Predicate<in>
+
+Predicate<int> isLarge = Foo.IsLarge;
+Console.WriteLine(isLarge(3));  // False
+Console.WriteLine(isLarge(12)); // True
+
+class Foo
+{
+    public static bool IsLarge(int value) => value > 10;
+}
