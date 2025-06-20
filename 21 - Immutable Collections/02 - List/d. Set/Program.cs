e9f67a88cd25a2dc59cd94f5f83f566b9e806014
Rick Beerendonk <rick@beerendonk.com>
2025-06-20 08:54:06 +0200

    Add "Immutable Collections - List - Set" example.

---
+++ 21 - Immutable Collections/02 - List/d. Set/Program.cs
@@ -0,0 +1,21 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+// Collection expression
+ImmutableList<string> lst = ["A", "B", "C"];
+
+Console.WriteLine(lst[1]); // B
+
+ // ImmutableList does not allow direct modification: lst[1] = "D";
+lst = lst.SetItem(1, "D");
+
+foreach (string item in lst)
+{
+  Console.WriteLine(item);
+}
+
+// A
+// B
+// C
