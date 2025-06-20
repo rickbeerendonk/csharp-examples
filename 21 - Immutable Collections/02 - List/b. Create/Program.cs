eebf901af4862deac376055c4b012e9a1bc508f7
Rick Beerendonk <rick@beerendonk.com>
2025-06-20 08:51:25 +0200

    Add "Immutable Collections - List - Create" example.

---
+++ 21 - Immutable Collections/02 - List/b. Create/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+ImmutableList<int> lst = [1, 2, 4];
+
+Console.WriteLine(lst.Count); // 3
