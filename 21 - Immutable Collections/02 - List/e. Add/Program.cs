cf7cd9da65d8265c4f0cae2fefeba104b7e18fed
Rick Beerendonk <rick@beerendonk.com>
2025-06-20 10:28:29 +0200

    Add "Immutable Collections - List - Add" example.

---
+++ 21 - Immutable Collections/02 - List/e. Add/Program.cs
@@ -0,0 +1,20 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+ImmutableList<string> lst = ["A", "B", "C"];
+
+lst = lst.Add("D");
+lst = lst.Insert(1, "E"); // Inserts "E" at index 1
+
+foreach (string item in lst)
+{
+  Console.WriteLine(item);
+}
+
+// A
+// E
+// B
+// C
+// D
