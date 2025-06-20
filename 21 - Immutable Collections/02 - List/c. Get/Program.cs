8ce89b40b0874b60aea77a1344bfe796b2b51daf
Rick Beerendonk <rick@beerendonk.com>
2025-06-20 08:54:54 +0200

    Add "Immutable Collections - List - Get" example.

---
+++ 21 - Immutable Collections/02 - List/c. Get/Program.cs
@@ -0,0 +1,18 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+// Collection expression
+ImmutableList<string> lst = ["A", "B", "C"];
+
+Console.WriteLine(lst[1]); // B
+
+foreach (string item in lst)
+{
+  Console.WriteLine(item);
+}
+
+// A
+// B
+// C
