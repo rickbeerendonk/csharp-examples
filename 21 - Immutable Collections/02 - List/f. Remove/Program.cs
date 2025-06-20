3bc263b8a21cc1bd6e44a04ca5a4558810a1c4eb
Rick Beerendonk <rick@beerendonk.com>
2025-06-20 10:30:14 +0200

    Add "Immutable Collections - List - Remove" example.

---
+++ 21 - Immutable Collections/02 - List/f. Remove/Program.cs
@@ -0,0 +1,18 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+ImmutableList<string> lst = ["A", "B", "C", "D", "E"];
+
+lst = lst.RemoveAt(1); // Removes the second item
+lst = lst.Remove("D"); // Removes "D"
+
+foreach (string item in lst)
+{
+  Console.WriteLine(item);
+}
+
+// A
+// C
+// E
