bbecd51a56ca3c3331008ba782abe3c267c78065
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:24:11 +0200

    Add "Mutable Collections - List - RemoveAll" example.

---
+++ 20 - Mutable Collections/01 - List/i. RemoveAll/Program.cs
@@ -0,0 +1,16 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+// Collection expression
+List<string> lst = ["A", "C", "B", "C", "D"];
+
+lst.RemoveAll(item => item == "C"); // Removes all "C"
+
+foreach (string item in lst)
+{
+  Console.WriteLine(item);
+}
+
+// A
+// B
+// D
