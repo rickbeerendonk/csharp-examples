24a05d4b084caa1b2c4d075464b98d423224263d
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:28:23 +0200

    Add "Mutable Collections - List - Clear" example.

---
+++ 20 - Mutable Collections/01 - List/j. Clear/Program.cs
@@ -0,0 +1,14 @@
+﻿// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+// Collection expression
+List<string> lst = ["A", "B", "C"];
+
+lst.Clear(); // Removes all items
+
+foreach (string item in lst)
+{
+  Console.WriteLine(item);
+}
+
+// (no output)
