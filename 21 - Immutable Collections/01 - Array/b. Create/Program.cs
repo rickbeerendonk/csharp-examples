2c68e2fb3800ffea742021ebd74963560fa98e1e
Rick Beerendonk <rick@beerendonk.com>
2025-09-16 12:04:23 +0200

    Add "Immutable Collections - Array - Create" example.

---
+++ 21 - Immutable Collections/01 - Array/b. Create/Program.cs
@@ -0,0 +1,8 @@
+﻿// European Union Public License version 1.2
+// Copyright © 205 Rick Beerendonk
+
+using System.Collections.Immutable;
+
+ImmutableArray<int> arr = [1, 2, 4]; // ImmutableArray.Create(1, 2, 4);
+
+Console.WriteLine(arr.Length); // 3
