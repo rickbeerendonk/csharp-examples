3789f803863f0f5ebadba73714986062e6ca5f20
Rick Beerendonk <rick@beerendonk.com>
2025-10-18 22:00:04 +0200

    Add "Operations and Expressions - Arithmetic" example.

---
+++ 06 - Operators and Expressions/a. Arithmetic/Program.cs
@@ -0,0 +1,11 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+int a = 10;
+int b = 3;
+
+Console.WriteLine($"Addition: {a} + {b} = {a + b}");
+Console.WriteLine($"Subtraction: {a} - {b} = {a - b}");
+Console.WriteLine($"Multiplication: {a} * {b} = {a * b}");
+Console.WriteLine($"Division: {a} / {b} = {a / b}");
+Console.WriteLine($"Modulus: {a} % {b} = {a % b}");
