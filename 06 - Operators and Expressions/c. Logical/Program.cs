fb6c720578039c8847358a9bad9b515d38c4a8c6
Rick Beerendonk <rick@beerendonk.com>
2025-10-18 22:00:38 +0200

    Add "Operations and Expressions - Comparison" example.

---
+++ 06 - Operators and Expressions/c. Logical/Program.cs
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+bool a = true;
+bool b = false;
+
+Console.WriteLine($"AND: {a} && {b} = {a && b}");
+Console.WriteLine($"OR: {a} || {b} = {a || b}");
+Console.WriteLine($"NOT: !{a} = {!a}");
+Console.WriteLine($"NOT: !{b} = {!b}");
