33516024bdf9a81243ea165bff7de1cc243a2ae8
Rick Beerendonk <rick@beerendonk.com>
2025-10-18 22:46:31 +0200

    Add "Conditions - switch-when" example.

---
+++ 10 - Conditions/f. switch-when/Program.cs
@@ -0,0 +1,20 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+// Switch with when clauses for additional conditions
+
+int number = 15;
+
+string result = number switch
+{
+  < 0 => "Negative",
+  0 => "Zero",
+  > 0 and <= 10 => "Between 1 and 10",
+  > 10 and <= 20 when number % 5 == 0 => "Between 11 and 20, divisible by 5",
+  > 10 and <= 20 => "Between 11 and 20",
+  _ => "Greater than 20"
+};
+
+Console.WriteLine($"Number {number} is: {result}");
+
+// Number 15 is: Between 11 and 20, divisible by 5
