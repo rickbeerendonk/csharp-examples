3abc3034958b2c5b675d08235691991e15160683
Rick Beerendonk <rick@beerendonk.com>
2026-05-14 16:55:22 +0200

    Add "Statements - Declaration" example.

---
+++ 08 - Statements/a. Declaration/Program.cs
@@ -0,0 +1,22 @@
+// European Union Public License version 1.2
+// Copyright © 2026 Rick Beerendonk
+
+// Declaration statements declare and optionally initialize variables
+
+// Simple declaration
+int x;
+x = 10;
+Console.WriteLine($"x = {x}");
+
+// Declaration with initialization
+string name = "Amsterdam";
+Console.WriteLine($"name = {name}");
+
+// Multiple declarations
+int a = 1, b = 2, c = 3;
+Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+
+// Var declaration
+var number = 42;
+var text = "Hello";
+Console.WriteLine($"number = {number}, text = {text}");
