1762cc6ebfa1e1657c05e58796ef54d86a7f0e70
Rick Beerendonk <rick@beerendonk.com>
2026-06-30 09:09:07 +0200

    Add "Classes - Properties - Auto-Implemented" example.

---
+++ 13 - Classes/02 - Properties/c. Auto-Implemented/Program.cs
@@ -0,0 +1,26 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+// Auto-implemented properties
+
+Person person = new()
+{
+  Name = "Alexandra",
+  Age = 30
+};
+
+Console.WriteLine($"Name: {person.Name}");
+Console.WriteLine($"Age: {person.Age}");
+
+// Read-only auto property
+Console.WriteLine($"ID: {person.Id}");
+
+class Person
+{
+  // Auto-implemented properties
+  public string Name { get; set; } = "";
+  public int Age { get; set; }
+
+  // Read-only auto property (can only be set in constructor or initializer)
+  public string Id { get; } = Guid.NewGuid().ToString();
+}
